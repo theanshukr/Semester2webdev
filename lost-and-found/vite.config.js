f3d18f6f1bb2e228e30febbaa0c24ce8dd6f7f33
Theanshukr <imanshu2702@gmail.com>
2026-06-23 22:21:36 +0530

    Update homepage base path and deploy script to root URL

--- lost-and-found/vite.config.js
+++ lost-and-found/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Semester2webdev/lost-and-found/',
+  base: '/Semester2webdev/',
 })
